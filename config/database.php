9f50a470701893fce9cec29f22c2a3c51d291e6c
sebacruz1 <sebastiancruzpomar@gmail.com>
2026-01-08 13:07:24 -0300

    arreglo para que funcione con el middleware

--- config/database.php
+++ config/database.php
@@ -29,12 +29,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            // By default fall back to the main DB credentials so provisioning works
-            // locally without requiring a dedicated MySQL user. For production it's
-            // recommended to set explicit PROVISIONER_DB_* variables in your .env
             'database' => env('PROVISIONER_DB_DATABASE', 'mysql'),
-            /* 'username' => env('PROVISIONER_DB_USERNAME', 'tenant_provisioner'),
-            'password' => env('PROVISIONER_DB_PASSWORD', ''), */
             'username' => env('PROVISIONER_DB_USERNAME', env('DB_USERNAME', 'root')),
             'password' => env('PROVISIONER_DB_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8mb4',
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return redirect('/admin/tenants');
 });
 
+
 Route::get('/admin/tenants', [TenantsController::class, 'showAdminView'])->name('admin.tenants.index');
 Route::get('/admin/tenants/{id}', [TenantsController::class, 'show'])->name('admin.tenants.show');
 Route::post('/admin/tenants', [TenantsController::class, 'store'])->name('admin.tenants.store');
